fdcdd18f624bfa421fe77dadea680763205dab00
unknown <neetamourya24@gmail.com>
2021-12-20 00:54:41 +0530

    [Neeta mourya] add password 1 special char valid in UC8

--- src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
@@ -82,12 +82,17 @@ public class UserRegistrationProblem {
         {
             // Regex to check valid password.
 //        	^ represents starting character of the string.
+//        	(?=\\S+$) white spaces don’t allowed in the entire string. 
 //        	.{8, 20} represents at least 8 characters and at most 20 characters.
 //        	(?=.*[a-z]) represents a lower case alphabet must occur at least once.
 //        	 (?=.*[A-Z]) represents an upper case alphabet that must occur at least once.
 //        	 (?=.*[0-9]) represents a digit must occur at least once.
+//        	(?=.*[@#$%^&-+=()] represents a special character that must occur at least once.
 //        	$ represents the end of the string.
-            String regex = "^(?=\\S+$).{8,20}$"+"(?=.*[a-z])(?=.*[A-Z])"+"(?=.*[0-9])";
+            String regex ="^(?=.*[0-9])"
+                    + "(?=.*[a-z])(?=.*[A-Z])"
+                    + "(?=.*[@#$%^&+=])"
+                    + "(?=\\S+$).{8,20}$";
             // Compile the ReGex
             Pattern p = Pattern.compile(regex);
             // If the password is empty
