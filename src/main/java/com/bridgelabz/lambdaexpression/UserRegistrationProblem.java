dfbfe00f2db44c437c0e82d993b2cb4b43b3bd1c
unknown <neetamourya24@gmail.com>
2021-12-20 00:46:57 +0530

    [Neeta mourya] add password min 8 char valid in UC5

--- src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-    public static boolean isValidUserFirstName(String firstname)
+	public static boolean isValidUserFirstName(String firstname)
     {
         // Regex to check valid userfirstname.
         String regex = "^[A-Za-z]\\w{3,29}$";
@@ -70,15 +70,37 @@ public class UserRegistrationProblem {
         // Function to validate the usermobilenumnber
         public static boolean isValidMobileNo(String mobileNumber)  
         {  
-        //(0/91): number starts with (0/91)  
-        //[7-9]: starting of the number may contain a digit between 0 to 9  
-        //[0-9]: then contains digits 0 to 9  
         Pattern ptrn = Pattern.compile("^((\\+|00)(\\d{1,3})[\\s-]?)?(\\d{10})$");
         //the matcher() method creates a matcher that will match the given input against this pattern  
         Matcher match = ptrn.matcher(mobileNumber);  
         //returns a boolean value  
         return (match.find() && match.group().equals(mobileNumber));  
         }  
+        // Function to validate the userpassword
+     // Function to validate the password.
+        public static boolean isValidPassword(String password)
+        {
+            // Regex to check valid password.
+//        	^ represents starting character of the string.
+//        	.{8, 20} represents at least 8 characters and at most 20 characters.
+//        	$ represents the end of the string.
+            String regex = "^(?=\\S+$).{8,20}$";
+            // Compile the ReGex
+            Pattern p = Pattern.compile(regex);
+            // If the password is empty
+            // return false
+            if (password == null) {
+                return false;
+            }
+            // Pattern class contains matcher() method
+            // to find matching between given password
+            // and regular expression.
+            Matcher m = p.matcher(password);
+            // Return if the password
+            // matched the ReGex
+            return m.matches();
+        }
+      
     // main function
     public static void main(String[] args)
     {
@@ -90,5 +112,7 @@ public class UserRegistrationProblem {
         System.out.println(isValidUserEmail(str3));  
         String str4 ="+91-9919819801"; //usermobilenumnber
         System.out.println(isValidMobileNo(str4));  
+        String str5 ="BridgeLabz@123"; //userpassword
+        System.out.println(isValidPassword(str5)); 
     }   
 }
