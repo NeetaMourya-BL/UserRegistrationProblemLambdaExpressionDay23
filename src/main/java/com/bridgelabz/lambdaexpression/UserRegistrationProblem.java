fb74361dc289719978da2606bb9e673f5fb41501
unknown <neetamourya24@gmail.com>
2021-12-20 00:39:46 +0530

    [Neeta mourya] add last name valid in UC2

--- src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
+++ src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
@@ -4,34 +4,57 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-	// Function to validate the username
-    public static boolean isValidUsername(String name)
+	// Function to validate the userfirstname
+    public static boolean isValidUserFirstName(String firstname)
     {
-        // Regex to check valid username.
+        // Regex to check valid userfirstname.
         String regex = "^[A-Za-z]\\w{3,29}$";
   
         // Compile the ReGex
         Pattern p = Pattern.compile(regex);
   
-        // If the username is empty
+        // If the userfirstname is empty
         // return false
-        if (name == null) {
+        if (firstname == null) {
             return false;
         }
         // Pattern class contains matcher() method
-        // to find matching between given username
+        // to find matching between given userfirstname
         // and regular expression.
-        Matcher m = p.matcher(name);
-        // Return if the username
+        Matcher m = p.matcher(firstname);
+        // Return if the userfirstname
         // matched the ReGex
         return m.matches();
     }
-  
+  	// Function to validate the userlastname
+        public static boolean isValidUserLastName(String lastname)
+        {
+            // Regex to check valid userlastname.
+            String regex = "^[A-Za-z]\\w{3,29}$";
+      
+            // Compile the ReGex
+            Pattern p = Pattern.compile(regex);
+      
+            // If the userlastname is empty
+            // return false
+            if (lastname == null) {
+                return false;
+            }
+            // Pattern class contains matcher() method
+            // to find matching between given userlastname
+            // and regular expression.
+            Matcher m = p.matcher(lastname);
+            // Return if the userlastname
+            // matched the ReGex
+            return m.matches();
+        }
+    
     // main function
     public static void main(String[] args)
     {
-        // Test Case: 1
-        String str1 = "Neetamourya";
-        System.out.println(isValidUsername(str1));
+        String str1 = "Neeta";
+        System.out.println(isValidUserFirstName(str1));
+        String str2 = "Mourya";
+        System.out.println(isValidUserLastName(str2));
     }
 }
