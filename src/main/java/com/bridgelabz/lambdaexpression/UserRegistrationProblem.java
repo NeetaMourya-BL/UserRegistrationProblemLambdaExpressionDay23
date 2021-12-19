b1919a0b4d71cdf1c1c9c5f137875a97e0c3a46d
unknown <neetamourya24@gmail.com>
2021-12-20 00:36:36 +0530

    [Neeta mourya] add first name valid in UC1

---
+++ src/main/java/com/bridgelabz/lambdaexpression/UserRegistrationProblem.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.lambdaexpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+	// Function to validate the username
+    public static boolean isValidUsername(String name)
+    {
+        // Regex to check valid username.
+        String regex = "^[A-Za-z]\\w{3,29}$";
+  
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+  
+        // If the username is empty
+        // return false
+        if (name == null) {
+            return false;
+        }
+        // Pattern class contains matcher() method
+        // to find matching between given username
+        // and regular expression.
+        Matcher m = p.matcher(name);
+        // Return if the username
+        // matched the ReGex
+        return m.matches();
+    }
+  
+    // main function
+    public static void main(String[] args)
+    {
+        // Test Case: 1
+        String str1 = "Neetamourya";
+        System.out.println(isValidUsername(str1));
+    }
+}
