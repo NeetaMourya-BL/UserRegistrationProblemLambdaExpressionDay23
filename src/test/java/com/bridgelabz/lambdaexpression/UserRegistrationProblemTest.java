0b80aba2f2033921222c84c6eb90e17493081a93
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-06 13:55:46 +0530

    [Neeta mourya] added parameterised test in UC11 branch

--- src/test/java/com/bridgelabz/lambdaexpression/UserRegistrationProblemTest.java
+++ src/test/java/com/bridgelabz/lambdaexpression/UserRegistrationProblemTest.java
@@ -3,7 +3,41 @@ package com.bridgelabz.lambdaexpression;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
 public class UserRegistrationProblemTest{
+	private UserRegistrationProblem user;
+    private final boolean expected;
+    private String email;
+
+    @Before
+    public void initializeClass(){
+    	user = new UserRegistrationProblem();
+    }
+
+    public UserRegistrationProblemTest(UserRegistrationProblem user, boolean expected) {
+        this.user = user;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<boolean[]> regexList(){
+        return Arrays.asList(new boolean[][] {
+                {"abc@yahoo.com" != null,true} , {"abc-100@yahoo.com" != null,true} ,
+                {"abc.100@yahoo.com" != null,true} , {"abc111@abc.com" != null,true},
+                {"abc-100@abc.net" != null,true} , {"abc.100@abc.com.au" != null,true},
+                {"abc@1.com" != null,true} , {"abc@yahoo.com.com" != null,true},
+                {"abc+100@yahoo.com" != null,true} , {"abc@.com" != null,false}
+        });
+    }
+    @Test
+	public void testUserRegistration(){
+        Assert.assertEquals(expected,user.isValidUserEmail(email));
+    }
+	
 	
     boolean result;
 	UserRegistrationProblem userTest = new UserRegistrationProblem();
